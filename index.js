ee391bed99a33123a4213ac71f86514aebe445b2
davidvaughan86 <david.vaughan86@gmail.com>
2020-12-18 17:06:25 -0500

    movie routers and controllers

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const http = require("http");
 const express = require("express");
 const morgan = require('morgan')
 const es6Renderer = require('express-es6-template-engine')
-const data = require('./templates/data.json')
-console.log(data) // good practice to see if the data is loading
+// const data = require('./data.json')
+// console.log(data) // good practice to see if the data is loading
 
 const app = express();
 const server = http.createServer(app);
@@ -17,28 +17,35 @@ app.set('view engine', 'html');
 
 const PORT = 5000;
 const homeRouter = require('./routers/home');
+const movieRouter= require('./routers/movieRouter')
 
 
 app.use(logger);
 app.use('/', homeRouter);
+app.use('/movies' , movieRouter);
 
 
-app.get('/movies' , (req,res) => {
+// app.get('/movies' , (req,res) => {
+//     res.render('movies' , {
+//         locals: {
+//             movies: data
+//         }
+//     })
+// });
+
     // res.send(data)
-    const movieHtmlArray = [];
-    for (let d of data) {
-        movieHtmlArray.push(`<p> ${d.name} </p>`);
+    // const movieHtmlArray = [];
+    // for (let d of data) {
+    //     movieHtmlArray.push(`<p> ${d.name} </p>`);
     
-        }
-        const movieHtmlString = movieHtmlArray.join('');
-        console.log(movieHtmlString)
-        res.render('movies' , {
-            locals: {
-                movie: movieHtmlString
-            }
-        })
-    });
+    //     }
+    //     const movieHtmlString = movieHtmlArray.join('');
+        
+    //     console.log(movieHtmlString)
+    // const movieHtmlString = data.map(d => `<p>!!!${d.title}!!</p>`).join('')
+    // console.log(movieHtmlString)
 
+      
 
 
 // app.use('/' , homeRouter);
